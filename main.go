f585ec63f026402a2dd793e64b639f41cd2e0637
guojunwengit <1647797846@qq.com>
2023-05-12 17:54:39 +0800

    Site updated: 2023-05-12 17:54:37

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [3]int{1, 2, 3}
+	for k, v := range a {
+		if k == 0 {
+			a[0], a[1] = 100, 200
+			fmt.Println(a)
+		}
+		a[k] = 100 + v
+	}
+	fmt.Println(a)
+}
